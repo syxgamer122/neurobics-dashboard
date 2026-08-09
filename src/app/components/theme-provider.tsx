6e2581774f9d4dc73e279ea1a60682bb78b8c8d6
Nguyen Huu Manh <flagcom2011@gmail.com>
2026-08-09 11:11:19 +0700

    fix: bỏ qua cảnh báo eslint react-refresh trong theme-provider

--- src/app/components/theme-provider.tsx
+++ src/app/components/theme-provider.tsx
@@ -62,7 +62,7 @@ export function ThemeProvider({
     </ThemeProviderContext.Provider>
   );
 }
-
+// eslint-disable-next-line react-refresh/only-export-components
 export const useTheme = () => {
   const context = useContext(ThemeProviderContext);
 
